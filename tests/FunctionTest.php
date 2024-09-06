0304f12d24b49a77ec8e42cefeb36553a256ba77
Raul Hidalgo <rhidalgog@izzi.mx>
2024-09-06 16:53:26 -0600

    Test that incorrect results are not returned: usin multiple test methods

--- tests/FunctionTest.php
+++ tests/FunctionTest.php
@@ -9,4 +9,8 @@ class ExampleTest extends TestCase {
         $this->assertEquals(4, add(2, 2));
         $this->assertEquals(8, add(3, 5));
     }
+
+    public function testAddDoesNotReturnTheIncorrectSum() {
+        $this->assertNotEquals(5, add(2, 2));
+    }
 }
