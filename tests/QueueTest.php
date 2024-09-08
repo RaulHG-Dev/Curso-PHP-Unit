0df56bd09e2ad9892d670a450aa08b4b7d9f62be
Raúl Hidalgo Gaspar <r.hidalgo.dev@gmail.com>
2024-09-08 12:04:54 -0600

    Unit test a queue class

---
+++ tests/QueueTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class QueueTest extends TestCase
+{
+    public function testNewQueueIsEmpty() {
+        $queue = new Queue;
+
+        $this->assertEquals(0, $queue->getCount());
+    }
+
+    public function testAnItemIsAddedToTheQueue() {
+        $queue = new Queue;
+        $queue->push('Green');
+
+        $this->assertEquals(1, $queue->getCount());
+    }
+
+    public function testAnItemIsRemovedFromTheQueue() {
+        $queue = new Queue;
+        $queue->push('Green');
+
+        $item = $queue->pop();
+
+        $this->assertEquals(0, $queue->getCount());
+    }
+}
